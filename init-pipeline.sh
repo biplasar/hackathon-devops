816e1f88b530a47e8c62157fe7dc4af8fe8bfb04
Ritabrata Chakraborty <ritac041@in.ibm.com>
2020-03-29 20:04:11 +0530

    update

--- init-pipeline.sh
+++ init-pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+oc project ritac041-in
 oc process -f backend-pipeline-template.yaml --param-file backend.properties | oc apply -f -
 oc process -f frontend-pipeline-template.yaml --param-file frontend.properties | oc apply -f -
 
--- setup.sh
+++ setup.sh
@@ -11,11 +11,14 @@ then
       exit
 fi
 
+####### Create generic secret for code fetch within pipeline
 oc create secret generic health-planner-github-repo-access-secret --from-literal username=$1 --from-literal password=$2
 oc annotate secret health-planner-github-repo-access-secret build.openshift.io/source-secret-match-uri-1=https://github.com/rito206868/*
 
+#####Creating Jenkins Ephimeral instance
 oc new-app jenkins-ephemeral
 
+#####Creating Mongodb persistent database
 oc create -f ocs-mongodb-perst.yaml
 oc new-app --name=mongo-healthplannerdb-36 --template=mongodb-persistent-ocs \
 							-p MONGODB_USER=root -p MONGODB_PASSWORD=root \
@@ -24,9 +27,11 @@ oc new-app --name=mongo-healthplannerdb-36 --template=mongodb-persistent-ocs \
 							-p DATABASE_SERVICE_NAME=healthplannerdb
 							
 
+#####Create RedHat secret to use latest images for build from RedHat registries
 oc create -f 12925791_ibm-redhat-connect-secret.yaml
 oc secrets link default 12925791-ibm-redhat-connect-pull-secret --for=pull
 oc secrets link builder 12925791-ibm-redhat-connect-pull-secret --for=pull
 oc secrets link jenkins 12925791-ibm-redhat-connect-pull-secret --for=pull
 
+#####Creating image stream for Java S2I build image
 oc import-image redhat-openjdk-18/openjdk18-openshift --from=registry.access.redhat.com/redhat-openjdk-18/openjdk18-openshift --confirm
